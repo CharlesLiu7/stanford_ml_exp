9a0869f3b9a869644b4eb2d4161e5e330db92958
CharlesLiu7 <haung7tjsl@gmail.com>
2016-08-30 09:09:49 +0800

    exp3: finish nn

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,6 +21,9 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+layer2 = sigmoid([ones(m,1), X]*Theta1');
+layer3 = sigmoid([ones(m,1), layer2]*Theta2');
+[~, p] = max(layer3, [], 2);
 
 
 
