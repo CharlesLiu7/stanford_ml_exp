91019d78fdc81d62eaf2ffb8330f21e393006ca4
CharlesLiu7 <haung7tjsl@gmail.com>
2016-09-04 10:11:03 +0800

    exp1: optional exercises finished

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -28,7 +28,11 @@ sigma = zeros(1, size(X, 2));
 
 
 
+mu = mean(X);
+X_norm = bsxfun(@minus, X, mu);
 
+sigma = std(X_norm);
+X_norm = bsxfun(@rdivide, X_norm, sigma);
 
 
 
--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,7 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    temp = sum(repmat((X * theta - y),1,2).* X)';
+    [~, s] = size(X); % s=2
+    temp = sum(repmat((X * theta - y),1,s).* X)';
     theta = theta - temp * alpha / m;
 
 
